154e35e8957173b96d5f6f9f9677d3bdacc3be12
Rodrigo R. da Silva <rodrigoridasi@gmail.com>
2021-03-25 22:20:22 -0300

    Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
 import Head from 'next/head';
 
+import styes from './home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>
-        Hello World
-      </h1>
+      <main className={styes.contentContainer}>
+        <section className={styes.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about <br /> the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publictions <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   )
 }
